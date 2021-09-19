8369ca5bb657cbd8b0390e3c30f1b11e62185746
Natália Campana <natalia.c.morais@gmail.com>
2021-09-19 12:49:29 -0300

    Change port configuration

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 
-let port = process.env.PORT;
-if (port || port === "")
-    port = 3000;
-
 const homeStartingContent = "aqui vai um pouco sobre mim: sou psicóloga formada em 2018, amo escrever e sou apaixonada pelos thrillers do Stephen King. Adquirir conhecimentos e me aventurar em novas áreas é algo que sempre me atraiu, por isso atualmente estou estudando programação e buscando entrar nesse mercado!"
 
 app.set("view engine", "ejs");
@@ -25,6 +21,10 @@ app.get("/curriculum", function(req, res) {
 app.get("/contact", function(req, res) {
     res.render("contact");
 });
+
+let port = process.env.PORT;
+if (port == null || port == "")
+    port = 3000;
 
 app.listen(port, function(){
     console.log("Server started on port " + port)
